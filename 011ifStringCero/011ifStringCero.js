8c54a46d984b2c39c9c62af6174ce95a0cb23f16
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-14 23:16:46 +0100

    Actualizado ej11

---
+++ 011ifStringCero/011ifStringCero.js
@@ -0,0 +1,9 @@
+"use strict"
+/*
+011ifStringCero.js/.html: ¿Se mostrará el alert?
+if ("0") {
+  alert( '¿me ejecuto o no?' );
+}
+
+En un if siempre se mostrara a no ser que sea 0. Es este caso se se muestra ya que es un string viene con las comillas
+*/
