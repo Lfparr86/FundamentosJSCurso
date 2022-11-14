176b7dcd4f00c10425804c892a4e03794e91c784
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-14 22:54:31 +0100

    Actualizado ej10

---
+++ 010comparaciones/010comparaciones.js
@@ -0,0 +1,12 @@
+"use strict"
+/*
+010comparaciones.js/.html: ¿Cuál será el resultado de las siguientes expresiones? 
+5 > 4 en este caso nos aparece true 5 es mayor que 4
+"apple" > "microsoft" en este caso aparece false ya que lo que esta comparando en el string es la primera y a es menor que m en la posicion del alfabeto
+"2" > "12" en este caso es true ya que dos es mayor que uno
+undefined == null en este caso da true ya que son lo mismo
+undefined === null en este caso da false
+null == "\n0\n" en este caso da false
+null === +"\n0\n" en este caso da false
+*/
+
